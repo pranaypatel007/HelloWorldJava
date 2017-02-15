afde5e49d63dfac155bee9d21d136e2d3326ac2d
Pranay Patel <pranay0710@gmail.com>
2017-02-15 16:14:16 +0530

    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
  
 public class HelloWorld
 {
-    public static void main(String args[]) throws IOException, InterruptedException
+    public static void main(String args[])
     {
 		for(int i=0;i<10;i++)
 		{
