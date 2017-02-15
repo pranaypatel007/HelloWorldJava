7c79e510bc4a49e3555eb1a16c7f4b9f7a485ee0
Pranay Patel <pranay0710@gmail.com>
2017-02-15 16:05:42 +0530

    HelloWold

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+ 
+public class HelloWorld
+{
+    public static void main(String args[]) throws IOException, InterruptedException
+    {
+		for(int i=0;i<10;i++)
+		{
+				System.out.println("Hi , Pranay");
+		}
+		
+		
+	  }
+	 
+}
